433aa7e0a988f45912c13a6f4c6706fdfbf8a79e
Tarun Kumar <tarunkumarpk33@gmail.com>
2024-01-16 12:01:38 +0530

    feat(initialsetup): Add lib configuration in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.tsx"),
+      name: "my-library",
+   		 
+      fileName: "outputFile",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
 })
